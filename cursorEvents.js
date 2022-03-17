530d331ac4290bf0dfb17b3b8cc979a436bc00c0
rakeshls <73830663+rakeshls@users.noreply.github.com>
2022-03-17 22:11:38 +0530

    Add files via upload

---
+++ cursorEvents.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+  },  
+  handleMouseEnterEvents: function () {
+    this.el.addEventListener("mouseenter", () => {
+      const id = this.el.getAttribute("id");
+      const postersId = [
+        "ChaChachaudharyAndSabu",
+        "lotpot-poster",
+        "suppandi",
+        "tenaliRama",
+      ];
+      if (postersId.includes(id)) {
+        const postersContainer = document.querySelector("#posters-container");
+        postersContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+        this.el.setAttribute("material", { color: "blue" });
+      }
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    this.el.addEventListener("mouseleave", () => {
+      const { selectedItemId } = this.data;
+      if (selectedItemId) {
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if (id == selectedItemId) {
+          el.setAttribute("material", { color: "#fff" });
+        }
+      }
+    });
+  },
+  
+});
